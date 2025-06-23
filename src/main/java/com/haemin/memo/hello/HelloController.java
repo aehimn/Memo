a3f187e33a5aebfc24e7219f7d78ded51504633c
김해민 <heaminkim1007@gmail.com>
2025-06-23 18:13:58 +0900

    [김해민] - 메모 프로젝트 생성 - thymeleaf, mysql, mybatis 설정

---
+++ src/main/java/com/haemin/memo/hello/HelloController.java
@@ -0,0 +1,21 @@
+package com.haemin.memo.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@ResponseBody
+	@GetMapping("/hello")
+	public String helloWorld() {
+		return "Hello World!!";
+	}
+	
+	@GetMapping("/hello/thymeleaf")
+	public String helloThymeleaf () {
+		return "hello/hello";
+	}
+
+}
