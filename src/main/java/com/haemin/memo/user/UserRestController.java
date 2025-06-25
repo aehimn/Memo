8651dd933e973cc37c21ea0b84a9409d6468dd28
김해민 <heaminkim1007@gmail.com>
2025-06-25 18:46:46 +0900

    [김해민] - 회원 가입 기능 - md5 해싱을 통한 비밀번호 암호화

---
+++ src/main/java/com/haemin/memo/user/UserRestController.java
@@ -0,0 +1,43 @@
+package com.haemin.memo.user;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.haemin.memo.user.service.UserService;
+
+// API 구성을 위한 Controller
+@RestController // @Controller + @ResponseBody
+@RequestMapping("/user")
+public class UserRestController {
+	
+	private final UserService userService;
+	
+	public UserRestController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@PostMapping("/join")
+	public Map<String, String> join(
+			@RequestParam String loginId
+			, @RequestParam String password
+			, @RequestParam String name
+			, @RequestParam String email) {
+		
+		Map<String, String> resultMap = new HashMap<>();
+		
+		if(userService.addUser(loginId, password, name, email)) {
+			resultMap.put("result", "success");
+		} else {
+			resultMap.put("result", "fail");
+		}
+		
+		return resultMap;
+		
+	}
+
+}
