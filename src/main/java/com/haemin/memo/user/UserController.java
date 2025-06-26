ad4667f15d6df8600d9780476f32a5994589ea85
김해민 <heaminkim1007@gmail.com>
2025-06-26 16:25:54 +0900

    Reapply "[김해민]"
    This reverts commit 85b7f57381bee38d49021ecf78677b20798eb8e9.

--- src/main/java/com/haemin/memo/user/UserController.java
+++ src/main/java/com/haemin/memo/user/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// View를 위한 Controller
 @RequestMapping("/user")
 @Controller
 public class UserController {
